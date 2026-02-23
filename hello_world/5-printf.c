7bceae74e84956321bcaf217397e95baa540919e
Harinaor <13048@holbertonstudents.com>
2026-02-23 14:10:49 +0000

    suppress accent characters

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * main -Entry point
- * 
- * printf afficher sans retour a la ligne inclus
- * return arr√te le programme proprement
+ * main - Entry point
  *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
