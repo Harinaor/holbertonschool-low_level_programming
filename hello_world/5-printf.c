c88d174a6a21083e0ebb0a8de3070ccc191a457b
Harinaor <13048@holbertonstudents.com>
2026-02-23 14:03:11 +0000

    printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * 
+ * printf afficher sans retour a la ligne inclus
+ * return arr√te le programme proprement
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
