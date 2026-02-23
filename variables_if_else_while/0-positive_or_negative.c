0cff1b5f035a0f8bc9db5b27e24f0002ec03acd7
Harinaor <13048@holbertonstudents.com>
2026-02-23 15:45:34 +0000

    share space against tab

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -8,18 +8,18 @@
 /* main - generates a random number and prints if it is positive, negative, or zero */
 int main(void)
 {
-    int n;
+	int n;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-    /* check and print if n is positive, negative, or zero */
-    if (n > 0)
-        printf("%d is positive\n", n);
-    else if (n == 0)
-        printf("%d is zero\n", n);
-    else
-        printf("%d is negative\n", n);
+/* check and print if n is positive, negative, or zero */
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
 
-    return (0);
+	return (0);
 }
