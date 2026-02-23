6f28d846f8cf279f439ec474c1f5a08a0f391238
Harinaor <13048@holbertonstudents.com>
2026-02-23 21:52:27 +0000

    esquisse program

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -10,7 +10,19 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	
-	
+ 	prinf("Last digit of %d is %d", n, n % 10);
+	{
+		if (n % 10 > 5){
+			printf("the string and is greater than 5");
+		}
+		else if (n % 10 == 0){
+			printf("the string and is 0");
+		}
+		else (n % 10 > 0 && n % 10 < 6){
+	       		printf("the string and is less than 6
+			and not 0");
+		}
+	}
 
 	return (0);
 }
