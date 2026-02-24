fc6b5565232f5f9c217764af1b127cd907433b5e
Harinaor <13048@holbertonstudents.com>
2026-02-24 12:03:24 +0000

    The last error Betty

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -4,8 +4,8 @@
 
 /**
  * main - calcul last digit of ramdom number and print whether
-
  * number is greater than 5 or is 0 or is less than 6 and  not 0
+ * return - print the last digit of the ramdom number 
  */
 int main(void)
 {
@@ -18,6 +18,7 @@ int main(void)
 		if (n % 10 > 5)
 		{
 			printf("the string and is greater than 5");
+		}
 		else if (n % 10 == 0)
 		{
 			printf("the string and is 0");
