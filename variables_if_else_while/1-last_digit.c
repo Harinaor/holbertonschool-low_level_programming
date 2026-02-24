c39968a419f4602d732187eb9d3a7e633d8e6b2d
Harinaor <13048@holbertonstudents.com>
2026-02-24 13:11:05 +0000

    Return:

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,30 +1,30 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
- * main - calcul last digit of ramdom number and print whether
- * number is greater than 5 or is 0 or is less than 6 and  not 0
- * return - print the last digit of the ramdom number 
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
+	int Lastdigit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-		printf("Last digit of %d is %d", n, n % 10);
+	Lastdigit = n % 10
+
+	if (Lastdigit > 5)
 	{
-		if (n % 10 > 5)
-		{
-			printf("and is greater than 5");
-		}
-		else if (n % 10 == 0)
-		{
-			printf("and is 0");
-		}
-		else
-			printf("and is less than 6 and not 0");
+		printf("Lastdigit of %d is %d and is greater than 5", n, Lastdigit);
+	}
+	if (Lastdigit == 0)
+	{
+		printf("Lastdigit of %d is %d and is 0", n, Lastdigit);
+	}
+	if (Lastdigit < 6 && Lastdigit != 0)
+	{
+		printf("Lastdigit of %d is %d and is less than 6 and not 0", n, Lastdigit);
 	}
 	return (0);
 }
