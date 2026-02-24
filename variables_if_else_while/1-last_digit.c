310131f944b85c2f18e8f7acc145b66f9ac69679
Harinaor <13048@holbertonstudents.com>
2026-02-24 11:41:36 +0000

    gcc + betty

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 
 /**
  * main - calcul last digit of ramdom number and print whether
- 
+
  * number is greater than 5 or is 0 or is less than 6 and  not 0
  */
 int main(void)
@@ -13,20 +13,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
- 	prinf("Last digit of %d is %d", n, n % 10);
+		printf("Last digit of %d is %d", n, n % 10);
 	{
-		if (n % 10 > 5){
+		if (n % 10 > 5)
+		{
 			printf("the string and is greater than 5");
-		}
-		else if (n % 10 == 0){
+		else if (n % 10 == 0)
+		{
 			printf("the string and is 0");
 		}
-		else (n % 10 > 0 && n % 10 < 6){
-	       		printf("the string and is less than 6
-			and not 0");
-		}
+		else
+			printf("the string and is less than 6 and not 0");
 	}
-
 	return (0);
 }
