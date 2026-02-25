8e693d7b01f9f631a12787c039ab0b561a7e787c
Harinaor <13048@holbertonstudents.com>
2026-02-25 12:34:13 +0000

    putchar _putchar and main

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,14 +1,23 @@
 #include "main.h"
+
 /**
- * main - print putchar
+ * main - Entry point
  *
- * Return: 0 (Success)
+ * Description: Prints _putchar followed by a new line
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int x='H';
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-    _putchar(x);
-    _putchar('\n');
-    return(0);
+	return (0);
 }
