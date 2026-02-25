ce80abb05f0e72062c458e19d6fd848d5a290544
Harinaor <13048@holbertonstudents.com>
2026-02-25 20:33:15 +0000

    5-sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -3,21 +3,25 @@
 /**
  * print_sign - Montre le signe d'un nombre
  * @n: Le caractère à vérifier (sous forme de nombre).
- *
- * Return: 1 si positif 0 si nul -1 si négatif
+ * Return: 1 et print + si positif
+ * 0 et print 0 si nul
+ * -1 et print - si négatif
  */
 int print_sign(int n)
 {
-	if (n > '0')
+	if (n > 0)
 	{
+		_putchar('+');
 		return (1);
 	}
-	else if (n < '0')
+	else if (n < 0)
 	{
+		_putchar('-');
 		return (-1);
 	}
 	else
 	{
+		_putchar('0');
 		return (0);
 	}
 }
