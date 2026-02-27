00c1706415438056cde35cd28e92d88d9ba331f8
Harinaor <13048@holbertonstudents.com>
2026-02-27 22:20:06 +0100

    print square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square - imprime un carré
+ * @size: la taille du carré
+ * Return: un carré de la taille spécifiée
+ */
+void print_square(int size)
+{
+int column;
+int line;
+
+if (size <= 0)
+_putchar('\n');
+else 
+for (column = 0 ; column < size ; column++)
+{
+for (line = 0 ; line < size ; line++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
