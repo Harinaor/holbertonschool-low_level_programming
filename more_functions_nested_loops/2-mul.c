c31ca6fd9b940d4bc095b884e776e63cd3ac0431
Harinaor <13048@holbertonstudents.com>
2026-02-27 12:20:47 +0100

    multiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - ecrire une fonction qui multiplie deux entiers.
+ * @a: le premier entier
+ * @b: le deuxieme entier
+ * Return: le resultat de la multiplication de a et b
+ */ 
+int mul(int a, int b)
+{
+	return (a * b);
+}
