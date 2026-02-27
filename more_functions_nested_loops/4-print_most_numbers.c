1ae10569669927a2ea5ea4f7d36e6169d96da978
Harinaor <13048@holbertonstudents.com>
2026-02-27 13:36:42 +0100

    betty to correct program not main

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,15 +2,15 @@
 
 /**
  * print_most_numbers - prints numbers from 0 to 9 except 2 and 4
- * 
+ *
  * Return: 0 (success)
  */
 void print_most_numbers(void)
 {
 int x;
-for(x = 0; x <= 9; x++)
+for (x = 0; x <= 9; x++)
 {
-if(x != 2 && x != 4)
+if (x != 2 && x != 4)
 _putchar(x + '0');
 }
 _putchar('\n');
