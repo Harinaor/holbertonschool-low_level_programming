35fae1eccff5a23a6caa891ebc112531e9c38de8
Harinaor <13048@holbertonstudents.com>
2026-03-02 13:06:38 +0100

    all in one file calculator

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include "main.h"
 
 /**
  *  * main - afficher un menu avec une ligne de bienvenue
@@ -12,6 +11,8 @@
  */
 
 /*Programme 1 : Menu de calculatrice*/
+int boucle_menu(void);
+
 int main(void)
 {
 printf("Welcome to my calculator \n");
@@ -25,4 +26,31 @@ printf("0')' Quit \n");
 boucle_menu();
 return (0);
 }
+/**
+ * boucle_menu - répéter le menu jusqu'à la commande 0 pour exit
+ * Les données @tiroir: sert à repérer les entrées invalides
+ * un compteur @i: sert à itérer les boucles du menu
+ * Return: 0
+ */
 
+ /*Programme 2 : Boucle de menu de calculatrice*/
+int boucle_menu(void)
+{
+int tiroir;
+
+while (tiroir != 0)
+{
+printf("Enter your choice: \n");
+(scanf("%d", &tiroir));
+if (tiroir == 0)
+{
+printf("Bye \n");
+exit(0);
+}
+else if (tiroir < 0 || tiroir >= 5)
+printf("Invalid choice %d \n", tiroir);
+else
+printf("the option selected: %d \n", tiroir);
+}
+return (0);
+}
