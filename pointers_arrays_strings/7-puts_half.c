02f14778bfecda2b9159f49251825fd5a84a0cda
Harinaor <13048@holbertonstudents.com>
2026-03-04 22:00:31 +0000

    filescomingsoon

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * puts_half - affiche la moitié d'une chaîne,
+ * suivi d'une nouvelle ligne 
+ * Return: affiche la seconde moitié de la chaîne
+ */
+
+void puts_half(char *str);
+/* Si le nombre de caractères est étrange, 
+ * la fonction devrait afficher les derniers n caractères de la chaîne
+ * où n = (longueur de la chaine + 1) / 2 */
+ 
