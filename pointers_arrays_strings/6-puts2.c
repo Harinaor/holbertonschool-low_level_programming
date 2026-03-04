737ec1241f4d9b6b6838b8dbfb5563944f3d60be
Harinaor <13048@holbertonstudents.com>
2026-03-04 21:52:59 +0000

    print pairs

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,1 +1,28 @@
-void puts2(char *str);git
+#include "main.h"
+/**
+ * puts2 - fonction qui affiche un caractère sur 2, ça débute à 0
+ * et ça termine par un saut de ligne
+ * Une chaîne de caractère commence par 0, c'est donc les nombres pairs
+ * @str: le pointeur sur les blocs mémoire de chaque caractère
+ * Return: affiche les pairs d'une chaine de caractères
+ */
+
+void puts2(char *str)
+{
+int x = 0;
+
+while (str[x] != '\0')
+{
+_putchar(str[x]);
+x++;
+
+
+if (str[x] == '\0')
+break;
+else
+{
+x++;
+}
+}
+_putchar('\n');
+}
