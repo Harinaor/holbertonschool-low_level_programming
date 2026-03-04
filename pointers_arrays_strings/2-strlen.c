9187bd18905c138b3137873b7d2ec2150a0fe196
Harinaor <13048@holbertonstudents.com>
2026-03-04 10:56:26 +0000

    mario jump on bloc above lava = strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strlen - Ecrire une fonction qui renvoi la longueur
+ * d'une chaîne de caractères
+ * @s: J'ai modélisé ça par mario qui marche
+ * sur des blocs au dessus de la lave
+ * quand mario tombe sur le bloc vide,
+ * c'est équivalent à la longueur des blocs parcouru
+ * donc la longueur de la chaîne
+ * exclu le dernier byte null ('\0')
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+int longueur = 0;
+while (*s != '\0')
+{
+longueur++;
+s++;
+}
+return (longueur);
+}
