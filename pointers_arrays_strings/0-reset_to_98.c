3e24713da33d3ea0ff79c38d97eb4f21a9aa31bd
Harinaor <13048@holbertonstudents.com>
2026-03-04 08:56:10 +0100

    first files

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+/**
+ * Ecrire une fonction qui prend un pointeur
+ * vers un entier comme paramètre 
+ * et met à jour la valeur pointée par ce pointeur à 98. 
+ *Return: valeur pointée à 98
+*/
+void reset_to_98(int *n);
+{
+int paquet
+int contenu
+
+*paquet = &contenu;
+*paquet = 98; 
+return(paquet);
+}
