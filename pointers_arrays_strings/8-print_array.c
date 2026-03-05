c05e9714fe162ca7470ba961a622107e3fce18a1
Harinaor <13048@holbertonstudents.com>
2026-03-05 15:56:57 +0000

    files prepared

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * Ecrire une fonction qui affiche 
+ * n elements d'un tableau d'entier
+ * suivi d'une nouvelle ligne
+Write a function that prints n elements of an array of integers, followed by a new line.
+
+Prototype: void print_array(int *a, int n);
+where n is the number of elements of the array to be printed
+Numbers must be separated by comma, followed by a space
+The numbers should be displayed in the same order as they are stored in the array
+You are allowed to use printf
