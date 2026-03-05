4a09e87db5e023f05820bc4938b9afb36a1699a9
Harinaor <13048@holbertonstudents.com>
2026-03-05 13:38:27 +0000

    betty correction

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- * _puts - Ecrit une fonction qui affiche une chaîne de caractères, 
+ * _puts - Ecrit une fonction qui affiche une chaîne de caractères,
  * suivi par \n vers stdout la sortie standard.
- * Concrètement la fonction permet d'afficher une phrase et de passer à la ligne.
+ * Concrètement la fonction permet d'afficher
+ * une phrase et de passer à la ligne.
+ * @c: un caractère
  * Return: chaîne affichée
  */
 int _putchar(char c);
 
 void _puts(char *str)
 {
-while ( *str != '\0')
+while (*str != '\0')
 {
 _putchar(*str);
 str++;
