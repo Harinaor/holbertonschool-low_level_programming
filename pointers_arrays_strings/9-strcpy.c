a7561b05fd7fa559af1b402b10708df4cccedd9d
Harinaor <13048@holbertonstudents.com>
2026-03-05 22:04:37 +0000

    files prepared

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * _strcpy - copie la chaine pointé par src
+ * dans le buffer pointé par dest
+ * incluant \0
+ * Return: le pointeur sur dest
+ */
+char _strcpy(char *dest, char *src);
