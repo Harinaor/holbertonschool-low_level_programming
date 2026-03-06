dc0aa41cacd31ade69a3dc50ea86f9d330e84885
Harinaor <13048@holbertonstudents.com>
2026-03-06 15:44:59 +0000

    files prepared

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,10 @@
+/**
+ * *_strncat - ecrit une fonction qui concatene partiellement deux chaines
+ * @n: nombre de caractère à copier depuis src
+ * @dest: buffer de reception de la copie
+ * @src: string source
+ * Return: concatenation partielle
+ */
+
+char *_strncat(char *dest, char *src, int n);
+
