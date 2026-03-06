099a53cf899f2e4b6f188ac4b7954fcbba6ace62
Harinaor <13048@holbertonstudents.com>
2026-03-06 12:25:49 +0000

    betty and betty-doc checked

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,9 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _strcpy - copie la chaine pointé par src
+ * *_strcpy - copie la chaine pointé par src
  * dans le buffer pointé par dest
  * incluant \0
+ * @src: la chaîne à copier
+ * @dest: le buffer dest
  * Return: le pointeur sur dest
  */
-char _strcpy(char *dest, char *src);
+char *_strcpy(char *dest, char *src)
+{
+char *ptr = dest;
+
+while (*src)
+*dest++ = *src++;
+*dest = '\0';
+return (ptr);
+}
