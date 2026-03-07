a7a1766b3454b42b2bd1209a29bb4eea555c6cfd
Harinaor <13048@holbertonstudents.com>
2026-03-07 13:17:58 +0000

    first try

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,10 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+
 /**
  * *_strncat - ecrit une fonction qui concatene partiellement deux chaines
  * @n: nombre de caractère à copier depuis src
+ * n est un entier limiteur
  * @dest: buffer de reception de la copie
  * @src: string source
  * Return: concatenation partielle
  */
 
-char *_strncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0;
+int j = 0;
+
+while (dest[i] != '\0')
+{
+i++;
+}
+
+while (src[j] != '\0')
+{
+if (j >= n)
+{
+dest[i] = src[j];
+i++;
+j++;
+dest[i] = n;
+printf("la valeur de i est %d \n", i);
+printf("la valeur de dest[i] est %d \n", dest[i]);
+printf("la valeur de n est %d \n", n);
+printf("la valeur de j est %d \n", j);
+}
+else
+{
+dest[i] = src[j];
+i++;
+j++;
+printf("la valeur de i est %d \n", i);
+printf("la valeur de dest[i] est %d \n", dest[i]);
+printf("la valeur de j est %d \n", j);
+}
+}
+dest[i] = '\0';
+
+return (dest);
+}
 
