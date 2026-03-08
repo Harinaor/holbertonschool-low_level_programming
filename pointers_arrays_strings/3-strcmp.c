41f1479ee0dfada5b5b6225e3ecdbd92799a47ca
Harinaor <13048@holbertonstudents.com>
2026-03-08 07:12:45 +0000

    string comparison

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - ecrit une fonction qui compare 2 chaines
+ * @s1: chaine 1
+ * @s2: chaine 2 
+ * Return < 0 si s1 < s2, 0 si s1 == s2, > 0 si s1 > s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int i = 0;
+
+while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+{
+i++;
+}
+return (s1[i] - s2[i]);
+}
