5e79da0ff2de21717447b83e81e4995e03b45244
Harinaor <13048@holbertonstudents.com>
2026-03-10 21:35:05 +0100

    malloc checked

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,20 +1,23 @@
+#include "main.h"
+#include <stdlib>
+
 /**
- * *malloc_checked - Ecrire une fonction qui loue de la mémoire avec malloc
- * 
- * Return: mem un pointer vers la mémoire allouée
+ * *malloc_checked - Ecrire une fonction qui alloue de la mémoire avec malloc
+ * @b: un entier positif
+ * Return: mem un pointer vers la mémoire allouée en hexadécimal
  */
 
 void *malloc_checked(unsigned int b)
 {
 /*STACK*/
-
+void *mem;
 /*HEAP*/
-
+mem = malloc(b);
 
 /*si malloc échoue, malloc_checked doit causer*/ 
 /*une fin de process avec une valeur status de 98*/
-if (malloc == NULL)
-return(98);
+if (mem == NULL)
+exit(98);
 
 return(mem);
 }
