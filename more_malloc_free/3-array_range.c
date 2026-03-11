a4063b924f523e4a592ea853d0af646e57f59ce9
Harinaor <13048@holbertonstudents.com>
2026-03-11 16:23:48 +0100

    array range

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -1,28 +1,40 @@
 #include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
 
 /**
  * *array_range - créé un tableau (ligne) d'entiers
- * le tableau créé doit contenir toutes les 
- * valeurs de min (inclus) à max (inclus) 
+ * le tableau créé doit contenir toutes les
+ * valeurs de min (inclus) à max (inclus)
+ * @min: entier minimum inclus dans le tableau
+ * @max: entier maximum inclus dans le tableau
  * ordonnées de min à max
  * Return: le pointer du tableau créé
  */
-Write a function that creates an array of integers.
-
-int *array_range(int min, int max);
+int *array_range(int min, int max)
 {
 /*STACK*/
-
+int i;
+int *ptr;
+int size = 0;
+/*Quels sont les dimensions*/
+/*Quelles sont les conditions aux limites*/
+/*Y a t-il des cas non pertinents*/
+/*si min ou max accueillent des char ou des float ou double*/
+if (min > max)
+return (NULL);
 
 /*HEAP*/
+size = (max - min + 1);
 
+ptr = malloc(size *sizeof(int));
 
-if (min > max) 
+/* Y-a t-il des boucles?*/
+
+if (ptr == NULL)
 return (NULL);
 
-if (malloc fails) 
-return (NULL);
-
+for (i = 0 ; i <= (size - 1) ; i++)
+ptr[i] = 0 + i;
+return (ptr);
 }
-
-
