6d90e1382d33b683e2351f2fd363fbdbfe616d05
Harinaor <13048@holbertonstudents.com>
2026-03-11 13:50:41 +0100

    add README.md

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *array_range - créé un tableau (ligne) d'entiers
+ * le tableau créé doit contenir toutes les 
+ * valeurs de min (inclus) à max (inclus) 
+ * ordonnées de min à max
+ * Return: le pointer du tableau créé
+ */
+Write a function that creates an array of integers.
+
+int *array_range(int min, int max);
+{
+/*STACK*/
+
+
+/*HEAP*/
+
+
+if (min > max) 
+return (NULL);
+
+if (malloc fails) 
+return (NULL);
+
+}
+
+
