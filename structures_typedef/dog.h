9cdd5021dd54c40188821f0608f4ba3952c9e6b6
Harinaor <13048@holbertonstudents.com>
2026-03-12 14:24:15 +0100

    struct dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+#include "main.h"
+
+/**
+ * struct dog - regroupe les caractéristiques de chiens
+ * @name: nom du chien
+ * @age: age du chien
+ * @owner: propriétaire du chien
+ * Return: rien
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
