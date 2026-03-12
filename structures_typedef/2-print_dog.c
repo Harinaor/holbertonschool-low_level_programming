a69c57431979d6bf1ffa783c6bc7b7f112ae2704
Harinaor <13048@holbertonstudents.com>
2026-03-13 00:53:00 +0100

    print dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,36 @@
+#include "dog.h"
+#include "main.h"
+#include <stdio.h>
+
+#include "dog.h"
+
+void print_dog(struct dog *d);
+
+/**
+ * print_dog - Ecrire une fonction qui affiche un struct dog
+ * @d: identité numérique du chien
+ * Return: rien
+ */
+
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+return;
+
+printf("Dog file:\n");
+
+if (d->name != NULL)
+	printf("Name: %s\n", d->name);
+else
+	{
+	printf("Name: (nil)\n");
+	}
+	printf("Age: %.1f\n", d->age);
+
+if (d->owner == NULL)
+	printf("Owner: (nil)\n");
+else
+	printf("Owner: %s\n", d->owner);
+
+return;
+}
