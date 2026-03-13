230cf3e5929eaf6ed533f5d81faef57d0fc1af47
Harinaor <13048@holbertonstudents.com>
2026-03-13 01:01:19 +0100

    dog_t

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,14 +7,16 @@
  * @age: age du chien
  * @owner: propriétaire du chien
  */
+
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+char *name;
+float age;
+char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
 
 #endif
