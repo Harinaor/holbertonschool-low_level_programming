3e219b2d0ad9171f9a728a7f97d9561406a58c18
Harinaor <13048@holbertonstudents.com>
2026-03-16 16:51:12 +0100

    array iterator v1 checked and sanity

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,31 +1,29 @@
 #include "function_pointers.h"
+#include <stddef.h>
 
 /**
- * Ecrire une fonction qui execute une autre fonction
+ * array_iterator - array_interator est une fonction appelle
+ * un tableau d'un certaine taille et lui
+ * applique une action qui est un appel de fonction.
  * en tant que paramètre sur chaque élément d'un tableau
  * @array: le tableau à parcourir
  * @size: la taille du tableau
  * @action: un pointeur vers la fonction à utiliser
  * Return: Rien.
  */
-*
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+size_t n;
 
-array_interator est une fonction appelle un tableau d'un certaine taille et lui
-applique une action qui est un appel de fonction.
+ /*J'enlève les cas où le tableau ne peux pas fonctionner*/
+if (array == NULL || size == 0 || action == NULL)
+return;
 
-if array == NULL|| size <= 0 || action == NULL
-return (void);
-
-for (size = 0 ;)
-array[size] = action; 
-// A simple subtraction function
-int subtract(int a, int b) {
-    return a - b;
-}
-
-void calc(int a, int b, int (*op)(int, int)) {
-    printf("%d\n", op(a, b));
-}
+/*je parcours le tableau et applique le */
+/*pointeur action à chaque case du tableau*/
+for (n = 0; n < size; n++)
+action(array[n]);
+/*mon pointeur de fonction doit trouver écho avec ce qui le déclenche*/
+/*dans 1-main.c les printf sont déjà là donc je n'en ai pas besoin*/
 }
