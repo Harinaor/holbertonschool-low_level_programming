30b7cc800570eaf2a36e79f0986a453d7e365b3b
Harinaor <13048@holbertonstudents.com>
2026-03-17 10:09:39 +0100

    betty correction

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -11,12 +11,12 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int n;
-	
+
 	if (cmp == NULL || size <= 0 || array == NULL)
 	return (-1);
 
 	for (n = 0; n < size - 1; n++)
 	if (cmp(array[n]) != 0)
-	return(n);
-return(-1);
+	return (n);
+return (-1);
 }
