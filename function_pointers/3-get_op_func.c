f01cdb0cb533301151c5b4136b6c8977baecca50
Harinaor <13048@holbertonstudents.com>
2026-03-17 23:05:34 +0100

    calculator with function pointers and description

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,23 +1,30 @@
 #include "3-calc.h"
 #include <string.h>
 
+/**
+ * get_op_func - permet d'obtenir une fonction
+ * liée à un opérateur
+ * @s: pointeur vers la fonction
+ * Return: NULL
+ */
+
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-    while (ops[i].op)
-    {
-        if (strcmp(ops[i].op, s) == 0)
-            return ops[i].f;
-        i++;
-    }
-    return NULL;
+	while (ops[i].op)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,26 +1,56 @@
 #include "3-calc.h"
 
+/**
+ * op_add - additionne deux entiers
+ * @num1: un entier
+ * @num2: un autre entier
+ * Return: num1 + num2
+ */
 int op_add(int num1, int num2)
 {
-    return num1 + num2;
+	return (num1 + num2);
 }
 
+/**
+ * op_sub - soustrait deux entiers
+ * @num1: un entier
+ * @num2: un autre entier
+ * Return: num1 - num2
+ */
 int op_sub(int num1, int num2)
 {
-    return num1 - num2;
+	return (num1 - num2);
 }
 
+/**
+ * op_mul - multiplie deux entiers
+ * @num1: un entier
+ * @num2: un autre entier
+ * Return: num1 * num2
+ */
 int op_mul(int num1, int num2)
 {
-    return num1 * num2;
+	return (num1 * num2);
 }
 
+/**
+ * op_div - divise deux entiers
+ * @num1: un entier
+ * @num2: un autre entier
+ * Return: num1 / num2
+ */
 int op_div(int num1, int num2)
 {
-    return num1 / num2;
+	return (num1 / num2);
 }
 
+/**
+ * op_mod - retourne le modulo de la division de deux entiers
+ * @num1: un entier
+ * @num2: un autre entier
+ * Return: num1 % num2
+ */
 int op_mod(int num1, int num2)
 {
-    return num1 % num2;
+	return (num1 % num2);
 }
